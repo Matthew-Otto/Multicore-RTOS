69834c0d06297554a2f78967904452f42e004b4b
Matthew Otto <106996253+Matthew-Otto@users.noreply.github.com>
2025-04-20 10:50:43 -0500

    change interpreter command

--- os/interpreter.c
+++ os/interpreter.c
@@ -6,7 +6,6 @@
 #include "../os/schedule.h"
 
 #define TIME        1564253156U
-#define RESET_TIME  1314397196U
 #define STATS       1781603564U
 #define SLEEP       2313861896U
 #define HELP        946971642U
@@ -14,8 +13,7 @@
 #define HELP_MESSAGE "\
 ### Commands:\r\n\
 help:           print this message\r\n\
-time:           print the current system time\r\n\
-reset_time:     reset the system time\r\n\
+time [reset]:   print (or reset) the current system time\r\n\
 stats:          print various OS diagnostics\r\n\
 sleep <ms>:     sleeps interpreter thread for specified ms\r\n\
 \r\n"
@@ -36,6 +34,7 @@ void interpreter(void) {
     uart_out_string("\x1B[2J\x1B[H");
     uart_out_string("Enter a command:\r\n");
     while (1) {
+        memset(input_buffer, 0, sizeof(input_buffer));
         uart_out_string("> ");
         uart_in_string(input_buffer, buffsize);
 
@@ -45,16 +44,26 @@ void interpreter(void) {
         char *args = input_buffer + strlen(cmd) + 1;
 
         switch (hash(cmd)) {
-            case TIME:
-                uint32_t time = get_mstime();
-                snprintf(input_buffer, buffsize, "Time is: %d\r\n", time);
-                uart_out_string(input_buffer);
-                break;
-
-            case RESET_TIME:
-                clear_mstime();
-                uart_out_string("System time reset.\r\n");
-                break;
+            case TIME: {
+                char s[20];
+                int32_t argcount = sscanf(args, "%s", &s);
+                if (argcount <= 0){
+                    uint32_t time = get_mstime();
+                    snprintf(input_buffer, buffsize, "Time is: %d\r\n", time);
+                    uart_out_string(input_buffer);
+                    break;
+                } else if (argcount == 1) {
+                    if (hash(s) == 1695364032U) {
+                        clear_mstime();
+                        uart_out_string("System time reset.\r\n");
+                        break;
+                    }
+                    uart_out_string("Error: invalid argument\r\n");
+                    break;
+                }
+                uart_out_string("Error: invalid number of arguments\r\n");
+                    break;
+            }
 
             case STATS:
                 uart_out_string("no stats yet\r\n");
